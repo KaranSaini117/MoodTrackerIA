b37136af9b280df9788a33bd492c2ed1858ace22
Karan Saini <karansaini117@gmail.com>
2021-11-06 17:18:09 -0700

    added some of the mysql logic

--- LogInPage/LogInPage.js
+++ LogInPage/LogInPage.js
@@ -3,14 +3,32 @@ const loginButton = document.getElementById("login-form-submit");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+    const formUsername = loginForm.username.value;
+    const formPassword = loginForm.password.value;
+    const dbPassword;
 
-    if (username === "user" && password === "password") {
+    var mysql = require('mysql');
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "yourusername",
+        password: "yourpassword",
+        database: "mydb"
+    });
+
+    alert("Connected"); 
+
+    var sql = 'SELECT * FROM login WHERE username = ?';
+    con.query(sql, [formUsername], function (err, result) {
+        if (err) throw err;
+        dbPassword = result[0].password;
+    });
+
+    if (formPassword === dpPassword) {
         alert("You have successfully logged in.");
         window.location.replace('../HomePage/HomePage.html');
-    } else if (username === "" && password == "") {
-        alert("Enter username and password");
+    } else if (formUsername === "" || formPassword === "") {
+        alert("Enter username or password");
     } else {
         alert("Invalid username or password.");
     }
