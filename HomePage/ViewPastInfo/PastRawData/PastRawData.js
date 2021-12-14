97120aef6272f0b840f09d6ddb8a9e4f303eab25
Karan Saini <karansaini117@gmail.com>
2021-12-13 19:23:36 -0800

    changed directory locations. ADDED PROTO-CALENDAR!!

--- HomePage/ViewPastInfo/PastRawData/PastRawData.js
+++ HomePage/ViewPastInfo/PastRawData/PastRawData.js
@@ -1,17 +1,25 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'user',
-  password: 'password',
-  database: 'database name'
-});
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected!');
-});
+// db connection, ignore for now
+// const mysql = require('mysql');
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'user',
+//   password: 'password',
+//   database: 'database name'
+// });
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log('Connected!');
+// });
 
-connection.connect();
+// connection.connect();
+// connection.end();
 
+/* fill in default/placeholder values */
 
+document.getElementById("date").value =  "10-12-2021";
+document.getElementById("hoursWorked").value =  "8";
+document.getElementById("hoursExercised").value =  "1.5";
+document.getElementById("hoursSlept").value =  "7";
+document.getElementById("moodRating").value =  "6.5";
+document.getElementById("reflection").value =  "Today was a decent day. I worked a lot, but I was able to get in adequate exercise. My goal is to sleep a bit more going forward.";
 
-connection.end();
